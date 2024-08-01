d54dfe4db146462bb182bc36fc9e86ec872a18a9
alex <louyuankang@osase.net>
2024-08-01 18:14:11 +0800

    完善register

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -11,7 +11,7 @@ const SECRET_KEY = process.env.SECRET_KEY || '75ZAAcVICvblfmTYnfXLYcXPASj0P3a8'
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body
   if (!username || !password) {
-    return commonRes.error(res, null, 'Username and password are required')
+    commonRes.error(res, null, 'Username and password are required')
   }
   try {
     const [rows, fields]: [QueryResult, FieldPacket[]] = await db.query(
@@ -20,13 +20,13 @@ export const login = async (req: Request, res: Response) => {
     )
 
     if ((rows as any[]).length === 0) {
-      return commonRes.error(res, null, 'Invalid username or password')
+      commonRes.error(res, null, 'Invalid username or password')
     }
     const user = (rows as any[])[0]
     const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if (!isPasswordValid) {
-      return commonRes.error(res, null, 'Invalid username or password')
+      commonRes.error(res, null, 'Invalid username or password')
     }
 
     const token = jwt.sign(
@@ -37,19 +37,24 @@ export const login = async (req: Request, res: Response) => {
 
     // res.json({ token });
 
-    return commonRes(res, { token })
+    commonRes(res, { token })
   } catch (error: any) {
-    return commonRes.error(res, null, error)
+    commonRes.error(res, null, error)
   }
 }
 
 export const register = async (req: Request, res: Response) => {
-  const { username, password } = req.body
+  const { username, password, email, check_password } = req.body
 
   if (!username || !password) {
-    return commonRes.error(res, null, '用户名和密码都是必填项')
+    commonRes.error(res, null, '用户名和密码都是必填项')
   }
-
+  if (check_password === password) {
+    commonRes.error(res, null, '两次输入的密码不一致')
+  }
+  if (email) {
+    commonRes.error(res, null, 'email是必填项')
+  }
   try {
     // 哈希密码
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -58,15 +63,15 @@ export const register = async (req: Request, res: Response) => {
     ])
     if ((rows as any[])?.length > 0) {
       commonRes.error(res, null, '不可以重复注册')
-      return
+
     }
     // 存储用户信息
     await db.query(
-      'INSERT INTO users (username, password, created_at) VALUES (?, ? , ?)',
-      [username, hashedPassword, dayjs().format()]
+      'INSERT INTO users (username, password, created_at, email) VALUES (?, ? , ? , ?)',
+      [username, hashedPassword, dayjs().format(), email]
     )
     commonRes(res, { message: '注册成功' })
   } catch (error: any) {
-    return commonRes.error(res, null, error)
+    commonRes.error(res, null, error)
   }
 }
