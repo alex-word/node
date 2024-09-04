7d06d04200c28015a9be060f080d1b67c0cee0cd
alex <louyuankang@osase.net>
2024-09-04 16:31:35 +0800

    新增hot search

--- src/pages/myHotSearch/config.ts
+++ src/pages/myHotSearch/config.ts
@@ -71,8 +71,130 @@ export const baiduConfig = {
     }
 }
 
-export const douyinConfig = {
-    url: "https://www.iesdouyin.com/web/api/v2/hotsearch/billboard/word/",
+export const toutiaoConfig = {
+    url: "https://www.toutiao.com/hot-event/hot-board/?origin=toutiao_pc&_signature=_02B4Z6wo00101IX8.KwAAIDD1wPctztkfhiF2PgAAEetND23C2dtf.6GgF4lXIpkEvJCoRMrRNxPrK0-.hhbZiqcw093hOHz4EtrLSImL1fhNZWpQlXqx7P6RxnbqXnsoIRYGRxoCOedce1ocf",
     config: {
+        "headers": {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "priority": "u=1, i",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "cookie": "__ac_nonce=066d7f14800d260f78504; __ac_signature=_02B4Z6wo00f01SmuRhwAAIDCe1FmB2KPveUpjkKAACy.62; tt_webid=7397681176716494348; gfkadpd=24,6457; ttcid=a1c96a4fd3754974b97706c085c03abd10; x-web-secsdk-uid=9aa05d19-b5f7-4529-901c-4c383a646b0c; local_city_cache=%E6%9D%AD%E5%B7%9E; s_v_web_id=verify_m0nfb2of_rTl4qjSK_HBXb_42NA_81Qe_sgyT0F62AVTx; csrftoken=acde349ab023268d6656449ab88738c7; tt_scid=0lt3ilsHvCJiuQPm3orzA9l28avOg8a6-sE5jFpBPH45PeRKKlck94JOzSJMDAC77e73; ttwid=1%7CvMFfX7bcpiRsYk7SfV9RYLBDTEKx-Yrj3ezkeLDUkaY%7C1725428704%7Ca2edad35448dd391954146fe79b121fbc6dbf637b0ef1128e002d97b8955d7f8",
+            "Referer": "https://www.toutiao.com/?&source=m_redirect",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+    }
+}
+
+export const tengxunConfig = {
+    url: "https://i.news.qq.com/gw/event/pc_hot_ranking_list?ids_hash=&offset=0&page_size=51&appver=15.5_qqnews_7.1.60&rank_id=hot",
+    config: {
+        "headers": {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "priority": "u=1, i",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "Referer": "https://news.qq.com/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+    }
+}
+
+export const sougouConfig = {
+    url: "https://hotlist.imtt.qq.com/Fetch",
+    config: {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "content-type": "application/json",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            "x-requested-with": "XMLHttpRequest",
+            "Referer": "https://123.sogou.com/",
+            "Referrer-Policy": "unsafe-url"
+        },
+        "body": "{\"md5s\":{\"main_md5\":\"14575d67715c3ac2b0cd9ddb4161f62178a9954a\",\"tencent_md5\":\"538532a03ff976e55ff0180a123496df4dc24b24\",\"weibo_md5\":\"ea2f99f00ba0e082a6491b06a57be08fa6dc9026\"}}",
+        "method": "POST"
+    }
+}
+export const tiebaConfig = {
+    url: "https://tieba.baidu.com/hottopic/browse/topicList",
+    config: {
+        "headers": {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+            "cookie": "XFI=e04f9370-6a89-11ef-8276-59465eefc8a0; XFCS=1F3FE390D82B9C928E5154CDE81C963A60F079992F4B672519F8465E63AB2080; XFT=ixW2VoLXdC3xChngTvw4V/GdchcvOBJyQ8wPZlOwKHs=; BAIDUID=1002E2FB9EB44025F59ED3D646924F38:FG=1; USER_JUMP=-1; Hm_lvt_292b2e1608b0823c1cb6beef7243ef34=1722849985; HMACCOUNT=E4F96E009E0C4907; video_bubble0=1; wise_device=0; newlogin=1; BIDUPSID=1002E2FB9EB44025F59ED3D646924F38; PSTM=1724914378; H_PS_PSSID=60278_60359_60601_60665_60677; BAIDUID_BFESS=1002E2FB9EB44025F59ED3D646924F38:FG=1; delPer=0; PSINO=5; BA_HECTOR=2l850h240k05848lahak0l0k3m2uia1jdfk721u; ZFY=VGyPfXUWNbq8krMNzbawJytCKD0lH4scN4lcAfQ2ZZQ:C; BAIDU_WISE_UID=wapp_1725432571963_600; ppfuid=undefined; XFI=d8ea6b00-6a89-11ef-b516-c548bed799f4; arialoadData=false; XFCS=EB982FFE05CD8927B34DFF043511C36BB65EF36874F183FCC99CEAE5B299B934; XFT=HBCb2cjxJok3mF9vHj2AMJm4rFckvXo+horD0LXv1EY=; Hm_lpvt_292b2e1608b0823c1cb6beef7243ef34=1725432588; RT=\"z=1&dm=baidu.com&si=027d228d-c09a-4919-99c5-67781ce5806b&ss=m0ni07hl&sl=3&tt=y7&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=27q&nu=2gks0cx0&cl=4gy&ul=toj&hd=u18\"",
+            "Referer": "https://tieba.baidu.com/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+    }
+}
+export const douyinConfig = {
+    url: "https://www.douyin.com/aweme/v1/web/hot/search/list/?device_platform=webapp&aid=6383&channel=channel_pc_web&detail_list=1&source=6&pc_client_type=1&version_code=170400&version_name=17.4.0&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=zh-CN&browser_platform=MacIntel&browser_name=Chrome&browser_version=127.0.0.0&browser_online=true&engine_name=Blink&engine_version=127.0.0.0&os_name=Mac+OS&os_version=10.15.7&cpu_core_num=8&device_memory=8&platform=PC&downlink=10&effective_type=4g&round_trip_time=100&webid=7402865880620746240&verifyFp=verify_lztfayrm_IG4pYQ13_1HbF_45k0_AwDQ_xYTyEbx4HL1X&fp=verify_lztfayrm_IG4pYQ13_1HbF_45k0_AwDQ_xYTyEbx4HL1X&msToken=w38z5gMxjBtKjtg0c8TfdkV22XRQFCVngsyMhyiuf5zBx_mR07WIXs1oEUVRH4li194L6i6KucnAAq01z_Odg5V9q9Zg2LwkeB3r5namaVNY3wq0mYvOp_Xzc4gclg%3D%3D&a_bogus=EymhQf0vdkgPvfyf53CLfY3q6vr3Yh%2Fs0trEMD2fmVV%2Fqy39HMY%2F9exoBQhvXbbjN4%2FkIeyjy4hbO3xprQA701wf7Wsx%2F2CZQg00t-P2so0j53intL6mE0hN4kb3SFlm5XNAEOJ0y75nFY00AocrmhK4bfebYyDWxp6Fa31o9E%3D%3D",
+    config: {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "priority": "u=1, i",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "cookie": "ttwid=1%7CAC-Vmkp6Hcf7fcbSxkVYuwwcLflK4UmqIdfL5ELonMg%7C1723614051%7C3271ae9e716a36e42b705d5d2cd1c5662155afdfaeb83697242feb2059b73a3c; UIFID_TEMP=04334f064e21198b2492613256b037a8641b36104347f0fcdf493d9e3675e398f5c2701f8b804f692b2d2f1fdf73d88277117ae1ee4c9fe34b5891f6933831e2697ef3e6d6e0c046f1ce0cb9ebe6b11c; csrf_session_id=094f2a637c200f6ac351c2313eb44619; s_v_web_id=verify_lztfayrm_IG4pYQ13_1HbF_45k0_AwDQ_xYTyEbx4HL1X; fpk1=U2FsdGVkX1++gsruPfCPOZPB0sdMWU2JHH8sN5uzU7ASRr4qBNZ3uL8AoV2Jxv/rH3vHO4zCarxXeK5Sg6g0dw==; fpk2=6a23775729fd6c068d20d383cbe27f9b; passport_csrf_token=e7f2ebf5d51a1bed0ada19c86a7e6698; passport_csrf_token_default=e7f2ebf5d51a1bed0ada19c86a7e6698; bd_ticket_guard_client_web_domain=2; UIFID=04334f064e21198b2492613256b037a8641b36104347f0fcdf493d9e3675e398b0ba6643a5f8e4e6fc7eaa46783c3b4b815c7c13c7ca464266f613705b7159c0ce11438e603ba6f7f324490fcb6ad1c370e7937a44f3d5cc20c1dbf2962836969f62fbfe249405a1987311e29faeb352a41b8649c033c9b1d96f13ddb677a9f038fcaa5a1cc3e1162a40d3ab8f190953b32f9eb5572ccb5a9eadc68e5641ab14; hevc_supported=true; dy_swidth=1920; dy_sheight=1080; FORCE_LOGIN=%7B%22videoConsumedRemainSeconds%22%3A180%7D; volume_info=%7B%22isUserMute%22%3Afalse%2C%22isMute%22%3Atrue%2C%22volume%22%3A0.5%7D; douyin.com; device_web_cpu_core=8; device_web_memory_size=8; stream_player_status_params=%22%7B%5C%22is_auto_play%5C%22%3A0%2C%5C%22is_full_screen%5C%22%3A0%2C%5C%22is_full_webscreen%5C%22%3A0%2C%5C%22is_mute%5C%22%3A1%2C%5C%22is_speed%5C%22%3A1%2C%5C%22is_visible%5C%22%3A1%7D%22; odin_tt=90dcf0f3cff03b0481ea90018d787c97adfaceffaa6c26304cfa180eb1c316d47ca6167ca663256caae1bef84fc068db2308dd77cc9ce5a13e83b8235a2d05c53312d16ec2d7e58a5abd4d5497e451cf; SEARCH_RESULT_LIST_TYPE=%22single%22; x-web-secsdk-uid=97c28d5e-65ed-4369-b817-95abe432e7b0; __ac_nonce=066d80c640053bb5a6f8b; __ac_signature=_02B4Z6wo00f01GV-K-wAAIDDN4EL9hZvmcRlXi9AAH-P5d; strategyABtestKey=%221725434992.996%22; xgplayer_user_id=295945264529; xg_device_score=7.409489771033123; xgplayer_device_id=47630001144; pwa2=%220%7C0%7C1%7C1%22; stream_recommend_feed_params=%22%7B%5C%22cookie_enabled%5C%22%3Atrue%2C%5C%22screen_width%5C%22%3A1920%2C%5C%22screen_height%5C%22%3A1080%2C%5C%22browser_online%5C%22%3Atrue%2C%5C%22cpu_core_num%5C%22%3A8%2C%5C%22device_memory%5C%22%3A8%2C%5C%22downlink%5C%22%3A10%2C%5C%22effective_type%5C%22%3A%5C%224g%5C%22%2C%5C%22round_trip_time%5C%22%3A50%7D%22; WallpaperGuide=%7B%22showTime%22%3A1725435188883%2C%22closeTime%22%3A0%2C%22showCount%22%3A1%2C%22cursor1%22%3A21%2C%22cursor2%22%3A2%7D; biz_trace_id=12738adf; bd_ticket_guard_client_data=eyJiZC10aWNrZXQtZ3VhcmQtdmVyc2lvbiI6MiwiYmQtdGlja2V0LWd1YXJkLWl0ZXJhdGlvbi12ZXJzaW9uIjoxLCJiZC10aWNrZXQtZ3VhcmQtcmVlLXB1YmxpYy1rZXkiOiJCQ0Z3OFlrWEdzNDNPYkFUbkdwWTZ3T3RhTTJ6R0pabXhCVG1ZS2hGS1h4eENFVHJBZ1QzSkE5dGJNaEhhTDVCR3BMeGY0NWI2UGtvQlFQalNzejVpbVU9IiwiYmQtdGlja2V0LWd1YXJkLXdlYi12ZXJzaW9uIjoxfQ%3D%3D; download_guide=%223%2F20240904%2F0%22; IsDouyinActive=true; home_can_add_dy_2_desktop=%220%22",
+            "Referer": "https://www.douyin.com/hot",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+    }
+}
+export const bilibiliConfig = {
+    url: "https://api.bilibili.com/x/web-interface/ranking/v2?rid=0&type=all",
+    config: {
+        "headers": {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "priority": "u=1, i",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "cookie": "buvid3=61535143-6732-EEC3-0494-409642DAAEA417395infoc; b_nut=1722318717; _uuid=6ED2D216-A234-6210B-A963-D23D28FF3DC317681infoc; home_feed_column=5; buvid_fp=bc0b2b49e5e6aa2e8d95254fd2579c94; buvid4=C87D61CF-8C80-EE20-35F3-79BBAE2EF4DA18549-024073005-MjWjeWCDLQ63Ww7T6Z8Ztg%3D%3D; rpdid=|(umRk~mkJuu0J'u~kkJkuRkk; header_theme_version=CLOSE; DedeUserID=3546565690002243; DedeUserID__ckMd5=f333c97f9e8348bb; CURRENT_QUALITY=80; enable_web_push=ENABLE; iflogin_when_web_push=1; SESSDATA=e147dbed%2C1739676157%2C229b0%2A81CjB8f21ZPj5LzJpl_evCoKJXP1YmOxXdwjExQNSv6eZb4GnE2yp4ZvkYSDMCxhYpIrQSVlFYOUZKd0t1RjRlMjNEMHZvR2V5SEIwak9fYU9iVEE2Vm5RMVFUNTZydG9mXy03d05wVTNzU3lZZXVKRFFhM1dEdkNjUHZobzJCVG95b213X1VLWUNRIIEC; bili_jct=81025bb3d636451b1dc52c7db2fe3762; sid=7efmdnpi; LIVE_BUVID=AUTO9317241241665563; is-2022-channel=1; bsource=search_google; CURRENT_FNVAL=4048; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjU1MjUxODEsImlhdCI6MTcyNTI2NTkyMSwicGx0IjotMX0.bk2_8ySjUGobWvOfqDb0-hiSpUxwlP8D3PGMN700TkI; bili_ticket_expires=1725525121; b_lsid=88151EAC_191BC093AE3; bp_t_offset_3546565690002243=973231892692205568; browser_resolution=1920-150",
+            "Referer": "https://www.bilibili.com/",
+            "Referrer-Policy": "no-referrer-when-downgrade"
+        },
+        "body": null,
+        "method": "GET"
     }
 }
--- src/pages/myHotSearch/index.ts
+++ src/pages/myHotSearch/index.ts
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const https = require('https');
 import commonRes from '../../../utils/commonRes';
 import silentHandle from '../../../utils/silentHandle';
-import { weiboConfig, baiduConfig, zhihuConfig } from './config'
+import { weiboConfig, baiduConfig, zhihuConfig, toutiaoConfig, sougouConfig, tengxunConfig, tiebaConfig, douyinConfig, bilibiliConfig } from './config'
 
 const app = express();
 const PORT = 3000;
@@ -38,59 +38,164 @@ const handleHotSearch = function (req: Request) {
                 config = baiduConfig.config
                 selector = '.category-wrap_iQLoo'; // 百度热搜选择器
                 break;
+            case 'toutiao':
+                url = toutiaoConfig.url;
+                config = toutiaoConfig.config
+                break;
+            case 'tengxun':
+                url = tengxunConfig.url;
+                config = tengxunConfig.config
+                break;
+            case 'sougou':
+                url = sougouConfig.url;
+                config = sougouConfig.config
+                break;
+            case 'tieba':
+                url = tiebaConfig.url;
+                config = tiebaConfig.config
+                break;
+            case 'douyin':
+                url = douyinConfig.url;
+                config = douyinConfig.config
+                break;
+            case 'bilibili':
+                url = bilibiliConfig.url;
+                config = bilibiliConfig.config
+                break;
             default:
                 throw new Error('Invalid source parameter');
         }
 
         try {
-            // 获取网页数据
-            fetch(url, config).then(res => res.text()).then(html => {
-                const $ = cheerio?.load(html);
-                const hotSearches: object[] = [];
-                $(selector).each((index: any, element: any) => {
-                    switch (source) {
-                        case 'weibo':
-                            if ($(element).find('span').text().trim() !== '') {
-                                const titleElement = $(element).find('a')
-                                const id = uuid.v4(titleElement.text().trim());
-                                const hot_metrics = $(element).find('span').text().trim().split(' ')
+            if (selector)
+                // 获取网页数据
+                fetch(url, config).then(res => res.text()).then(html => {
+                    const hotSearches: object[] = [];
+                    const $ = cheerio?.load(html);
+                    $(selector).each((index: any, element: any) => {
+                        switch (source) {
+                            case 'weibo':
+                                if ($(element).find('span').text().trim() !== '') {
+                                    const titleElement = $(element).find('a')
+                                    const id = uuid.v4(titleElement.text().trim());
+                                    const hot_metrics = $(element).find('span').text().trim().split(' ')
+                                    hotSearches.push({
+                                        hot_search_id: id,
+                                        hot_search_title: titleElement.text().trim(),
+                                        hot_search_href: 'https://s.weibo.com' + titleElement[0].attribs.href,
+                                        hot_metrics: hot_metrics[hot_metrics.length - 1],
+                                        source: 'weibo'
+                                    });
+                                }
+                                break;
+                            case 'baidu':
+                                const titleElement = $(element).find('.c-single-text-ellipsis')
                                 hotSearches.push({
-                                    hot_search_id: id,
+                                    hot_search_id: uuid.v4(titleElement.text().trim()),
                                     hot_search_title: titleElement.text().trim(),
-                                    hot_search_href: 'https://s.weibo.com' + titleElement[0].attribs.href,
-                                    hot_metrics: hot_metrics[hot_metrics.length - 1],
-                                    source: 'weibo'
+                                    hot_search_href: titleElement[0].parent.attribs.href,
+                                    hot_metrics: $(element).find('.hot-index_1Bl1a').text().trim(),
+                                    source: 'baidu'
                                 });
-                            }
-                            break;
-                        case 'baidu':
-                            const titleElement = $(element).find('.c-single-text-ellipsis')
-                            hotSearches.push({
-                                hot_search_id: uuid.v4(titleElement.text().trim()),
-                                hot_search_title: titleElement.text().trim(),
-                                hot_search_href: titleElement[0].parent.attribs.href,
-                                hot_metrics: $(element).find('.hot-index_1Bl1a').text().trim(),
-                                source: 'baidu'
-                            });
-                            break;
-                        case 'zhihu':
-                            hotSearches.push(...JSON.parse(element.children[0].data).initialState.topstory.hotList?.map(
-                                (item: ZhiHuItemProps) => (
+                                break;
+                            case 'zhihu':
+                                hotSearches.push(...JSON.parse(element.children[0].data).initialState.topstory.hotList?.map(
+                                    (item: ZhiHuItemProps) => (
+                                        {
+                                            hot_search_id: item.target.link.url.substring(31,),
+                                            hot_search_title: item.target.titleArea.text,
+                                            hot_search_href: item.target.link.url,
+                                            hot_metrics: item.target.metricsArea.text.substring(0, item.target.metricsArea.text.length - 2),
+                                            source: 'zhihu'
+                                        })))
+                                break;
+                            // hotSearches.push(...html.data?.map(
+                            //     (item: any) => (
+                            //         {
+                            //             hot_search_id: item.ClusterId,
+                            //             hot_search_title: item.Title,
+                            //             hot_search_href: item.Url,
+                            //             hot_metrics: item.HotValue,
+                            //             source: 'toutiao'
+                            //         })))
+                            default:
+                                break;
+                        }
+                    });
+                    resolve(hotSearches)
+                })
+            else {
+                fetch(url, config).then(res => res.text()).then(html => {
+                    const hotSearches: object[] = [];
+                    let hotList = []
+                    switch (source) {
+                        case 'toutiao':
+                            hotSearches.push(...JSON.parse(html).data?.map(
+                                (item: any) => (
                                     {
-                                        hot_search_id: item.target.link.url.substring(31,),
-                                        hot_search_title: item.target.titleArea.text,
-                                        hot_search_href: item.target.link.url,
-                                        hot_metrics: item.target.metricsArea.text.substring(0, item.target.metricsArea.text.length - 2),
-                                        source: 'zhihu'
+                                        hot_search_id: item.ClusterId,
+                                        hot_search_title: item.Title,
+                                        hot_search_href: item.Url,
+                                        hot_metrics: item.HotValue,
+                                        source: 'toutiao'
                                     })))
-                            break;
-
+                        case 'tengxun':
+                            hotList = JSON.parse(html).idlist?.[0]?.newslist?.slice(2)
+                            hotSearches.push(...hotList?.map((item: { hotEvent: { id: any; title: any; hotScore: any; }; surl: any; }) => ({
+                                hot_search_id: item.hotEvent.id,
+                                hot_search_title: item.hotEvent.title,
+                                hot_search_href: item.surl,
+                                hot_metrics: item.hotEvent.hotScore,
+                                source: 'sougou'
+                            })))
+                        case 'sougou':
+                            hotSearches.push(...JSON.parse(html).main?.map(
+                                (item: any) => (
+                                    {
+                                        hot_search_id: uuid.v4(item.title),
+                                        hot_search_title: item.title,
+                                        hot_search_href: item.url,
+                                        hot_metrics: item.score,
+                                        source: 'sougou'
+                                    })))
+                        case 'tieba':
+                            hotSearches.push(...JSON.parse(html).data?.bang_topic?.topic_list?.map(
+                                (item: any) => (
+                                    {
+                                        hot_search_id: uuid.v4(item.topic_id),
+                                        hot_search_title: item.topic_name,
+                                        hot_search_href: item.topic_url,
+                                        hot_metrics: item.discuss_num,
+                                        source: 'tieba'
+                                    })))
+                        case 'douyin':
+                            hotList = JSON.parse(html).data?.word_list?.slice(1)
+                            hotSearches.push(...hotList?.map(
+                                (item: any) => (
+                                    {
+                                        hot_search_id: uuid.v4(item.word),
+                                        hot_search_title: item.word,
+                                        hot_search_href: `https://www.douyin.com/search/${item.word}?type=general`,
+                                        hot_metrics: item.hot_value,
+                                        source: 'douyin'
+                                    })))
+                        case 'bilibili':
+                            hotList = JSON.parse(html).data?.list
+                            hotSearches.push(...hotList?.map(
+                                (item: any) => (
+                                    {
+                                        hot_search_id: uuid.v4(item.aid),
+                                        hot_search_title: item.title,
+                                        hot_search_href: item.short_link_v2,
+                                        hot_metrics: item.stat.view,
+                                        source: 'bilibili'
+                                    })))
                         default:
                             break;
                     }
-                });
-                resolve(hotSearches)
-            })
+                    resolve(hotSearches)
+                })
+            }
         } catch (error) {
             reject(error)
         }
